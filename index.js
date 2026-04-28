6d66cfc443ae69530573c92fd5b46076b06db472
copilotcdn7 <copilotcdn@proton.me>
2026-04-29 02:49:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 // Proxy middleware configuration
 const proxyOptions = {
-  target: 'https://cd.sevix.store', // Replace with your target server URL
+  target: 'https://play.razr.su', // Replace with your target server URL
   changeOrigin: true,
   ws: true, // Enable WebSockets proxying
   secure: true // Enable SSL/TLS (HTTPS) for the proxy connection
